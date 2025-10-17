448ac383cbd54623c51e75e01c950d9230a5c206
alokdangre <alokdangre@gmail.com>
2025-10-17 13:34:21 +0530

    added contract schema

---
+++ backend/app/models/contract.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.db import Base
+
+class Contract(Base):
+    __tablename__= "contracts"
+    id = Column(Integer, primary_key=True,index=True)
+    listing_id=Column(Integer,ForiengnKey("listings.id"))
+    buyer_id = Column(String, ForeignKey("users.id"))
+    status = Column(String,default="pending")
+    offer_price- Column(Float)
+    listing = relationship("Listing")
+    buyer = relationship("User")
