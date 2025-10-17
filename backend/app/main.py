b6b91c7b0aa03e1ce22738b1aa1ab26c890f08a9
alokdangre <alokdangre@gmail.com>
2025-10-17 13:58:08 +0530

    made a fastapi server

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.core.db import Base, engine
+from app.routers import listing_router, contract_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Oilseed Hedging Backend")
+
+app.include_router(listing_router.router, prefix="/listings", tags=["Listings"])
+app.include_router(contract_router.router, prefix="/contracts", tags=["Contracts"])
